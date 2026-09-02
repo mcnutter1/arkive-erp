a18706163cbd4ffed8959fdba90d0330cbceb680
mcnutter1 <robert.mcnutt@gmail.com>
2026-09-02 19:22:16 -0400

    ddf

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -7,9 +7,9 @@ cd "$ROOT_DIR"
 docker compose ps
 
 echo "[health] api"
-docker compose exec -T api node -e "fetch('http://localhost:4000/api/v1/health/liveness').then((r)=>process.exit(r.ok?0:1)).catch(()=>process.exit(1))"
+docker compose exec -T api node -e "fetch('http://localhost:4000/api/v1/health/liveness').then((r)=>process.exit(r.status < 500 ? 0 : 1)).catch(()=>process.exit(1))"
 
 echo "[health] web"
-docker compose exec -T web node -e "fetch('http://localhost:3000').then((r)=>process.exit(r.ok?0:1)).catch(()=>process.exit(1))"
+docker compose exec -T web node -e "fetch('http://localhost:3000').then((r)=>process.exit(r.status < 500 ? 0 : 1)).catch(()=>process.exit(1))"
 
 echo "[health] done"
